fb0a462914932e85985d179275b70a2f4629af01
muzard <arttu.kokki@gmail.com>
2022-09-04 18:40:05 +0300

    Add slider to pick size

---
+++ script.js
@@ -0,0 +1,10 @@
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo"); // Display the default slider value
+
+let sliderValueText = document.querySelector("#sliderValue")
+let sliderValue;
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  sliderValueText.textContent = `Grid size: ${this.value}x${this.value}`;
+  sliderValue = this.value;
+}
